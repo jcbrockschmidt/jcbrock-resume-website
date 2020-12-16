101398037bda70a7cd85d67e02652fba427c839a
Jaclyn Brockschmidt <jcbrockschmidt@gmail.com>
2020-12-15 21:44:31 -0800

    Add development script for deploying the website over SSH

---
+++ dev-scripts/deploy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Deploys the website to a server over SSH + rsync.
+#
+# Modify deploy.config with the appropriate username, server, and the directory
+# to host the website's files via.
+
+DIR=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
+
+# Load configuration
+. "$DIR/deploy.config"
+
+# Deploy website
+cd "$DIR/.."
+hugo && rsync -avz --delete public/ ${user}@${host}:${dest}
